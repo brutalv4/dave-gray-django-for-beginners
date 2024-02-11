a2d0d9ddca220bacbee336e59f18678d83cf18c3
Vladimir Bredikhin <brutal.v4@gmail.com>
2024-02-11 20:35:52 +0200

    Python Django Pages, URLs & Slugs

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 from .models import Post
 
 
@@ -6,3 +7,8 @@ from .models import Post
 def posts_list(request):
     posts = Post.objects.all().order_by("-date")
     return render(request, "posts/posts_list.html", {"posts": posts})
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, "posts/post_page.html", {"post": post})
